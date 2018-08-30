773e91a30fc3715242d9e1991ff1c81768e0ad0d
kikh <p13mout@ionio.gr>
2018-08-30 16:49:08 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -28,6 +28,10 @@ function init() {
   var mapContainer = document.getElementById('map-holder');
   geoJsonOutput = document.getElementById('geojson-output');
   downloadLink = document.getElementById('download-link');
+
+  //changing the stroke of the paths on mouseover
+  map.data.addListener('mouseover',function (event){map.data.overrideStyle(event.feature,{strokeWeight:5});});
+  map.data.addListener('mouseover',function (event){map.data.overrideStyle(event.feature,{strokeWeight:3});});
 }
 
 google.maps.event.addDomListener(window, 'load', init);
@@ -51,8 +55,4 @@ function bindDataLayerListeners(dataLayer) {
   dataLayer.addListener('removefeature', refreshGeoJsonFromData);
   dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
 }
-
-//changing the stroke of the paths on mouseover
-map.data.addListener('mouseover',function (event){map.data.overrideStyle(event.feature,{strokeWeight:5});});
-map.data.addListener('mouseover',function (event){map.data.overrideStyle(event.feature,{strokeWeight:3});});
 
